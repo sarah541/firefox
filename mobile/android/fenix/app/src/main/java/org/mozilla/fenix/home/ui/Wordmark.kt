4d1462efe987d85cabae2138e1ab42b781f6041d
Julie De Lorenzo <jdelorenzo@mozilla.com>
2026-05-14 18:46:28 +0000

    Bug 2039380:  Fix crash in private tab from missing sports widget resource
    Original Revision: https://phabricator.services.mozilla.com/D300446

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/Wordmark.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/home/ui/Wordmark.kt
@@ -15,6 +15,8 @@ import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.SemanticsPropertyKey
+import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
 import androidx.compose.ui.semantics.testTagsAsResourceId
@@ -23,24 +25,32 @@ import org.mozilla.fenix.R
 import org.mozilla.fenix.home.ui.HomepageTestTag.HOMEPAGE_WORDMARK_LOGO
 import org.mozilla.fenix.home.ui.HomepageTestTag.HOMEPAGE_WORDMARK_TEXT
 
+/**
+ * Semantic property for accessing a Composable item's current resource property.
+ */
+internal val ResourceId = SemanticsPropertyKey<Int>("ResourceId")
+internal var SemanticsPropertyReceiver.resourceId by ResourceId
+
 @Composable
 internal fun WordmarkLogo(
     onLogoClicked: () -> Unit,
     onLogoLongClicked: () -> Unit,
     isSportsWidgetEnabled: Boolean,
 ) {
+    val wordmarkResourceId = if (isSportsWidgetEnabled) R.attr.fenixWordmarkSportLogo else R.attr.fenixWordmarkLogo
     Image(
         modifier = Modifier
             .height(40.dp)
             .semantics {
                 testTagsAsResourceId = true
                 testTag = HOMEPAGE_WORDMARK_LOGO
+                resourceId = wordmarkResourceId
             }
             .combinedClickable(onClick = onLogoClicked, onLongClick = onLogoLongClicked)
             .padding(end = 10.dp),
         painter = painterResource(
             getAttr(
-                if (isSportsWidgetEnabled) R.attr.fenixWordmarkSportLogo else R.attr.fenixWordmarkLogo,
+                wordmarkResourceId,
             ),
         ),
         contentDescription = null,
